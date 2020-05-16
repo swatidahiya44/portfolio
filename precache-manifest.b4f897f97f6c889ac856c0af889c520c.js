599bb0c6ac4c308f17127fa42042b1d2659130d3
unknown <swatidahiya44@gmail.com>
2020-05-16 06:26:52 +0530

    Updates

---
+++ precache-manifest.b4f897f97f6c889ac856c0af889c520c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33357d7c85b98f89eb9a48fb64abeac4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "13be4fb0a8cbd8769f2b",
+    "url": "/portfolio/static/css/main.81dc9501.chunk.css"
+  },
+  {
+    "revision": "a66e973fb2df46474292",
+    "url": "/portfolio/static/js/2.f9581120.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/portfolio/static/js/2.f9581120.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13be4fb0a8cbd8769f2b",
+    "url": "/portfolio/static/js/main.1d02f524.chunk.js"
+  },
+  {
+    "revision": "8aceb13f6772a4d5a4b8",
+    "url": "/portfolio/static/js/runtime-main.7b598343.js"
+  },
+  {
+    "revision": "8f32bb717f8662210846394d07ce8e25",
+    "url": "/portfolio/static/media/IMG_1109.8f32bb71.jpg"
+  }
+]);
